5d89cc0092babb4b020a09f48c7becde91dc3171
Sherman Chen <uxdailygeek@gmail.com>
2019-12-23 18:49:33 +0800

    Completed the ApplicationCulture Example code.

---
+++ ApplicationCultureExample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationCultureExample
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow ()
+		{
+			InitializeComponent ();
+		}
+
+		private void CultureInfoSwitchButton_Click(object sender, RoutedEventArgs e)
+		{
+			Thread.CurrentThread.CurrentCulture = new CultureInfo((sender as Button).Tag.ToString());
+			lblNumber.Content = (123456789.42d).ToString("N2");
+			lblDate.Content = DateTime.Now.ToString();
+		}
+	}
+}
