955b180c7751b0f767bc3f14c56e5a69c2665081
Sherman Chen <uxdailygeek@gmail.com>
2019-12-26 15:06:25 +0800

    Completed the TextBox examples

--- TextBoxSelections/MainWindow.xaml.cs
+++ TextBoxSelections/MainWindow.xaml.cs
@@ -24,5 +24,14 @@ namespace TextBoxSelections
 		{
 			InitializeComponent ();
 		}
+
+		private void TextBox_SelectionChanged ( object sender, RoutedEventArgs e )
+		{
+			TextBox textBox = sender as TextBox;
+			txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
+			txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
+			txtStatus.Text += "Selected text: " + textBox.SelectedText + "'";
+
+		}
 	}
 }
